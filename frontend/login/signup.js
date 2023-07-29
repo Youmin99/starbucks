4a04531c6756ec79278a940c580fb8a76de4df24
youmin <105027461+futureyp@users.noreply.github.com>
2023-07-28 20:35:17 -0700

    Twilio

--- frontend/login/signup.js
+++ frontend/login/signup.js
@@ -1,7 +1,13 @@
 // 휴대폰 인증 토큰 전송하기
 const getValidationNumber = async () => {
   document.querySelector('#ValidationInputWrapper').style.display = 'flex'
-  console.log('인증 번호 전송')
+  const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+      .create({from: '+15557122661', body: 'Hi there', to: '+15558675310'})
+      .then(message => console.log(message.sid));
 }
 
 // 회원 가입 API 요청
