507ce58b9146d4da7c9cabf67d26100c05254ecd
youmin <105027461+futureyp@users.noreply.github.com>
2023-08-07 03:05:04 -0700

    API: POST /tokens/phone
    create and update token

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,9 @@ import {
     getWelcomeTemplate,
     sendTemplateToEmail,
 } from './email.js';
+import mongoose from 'mongoose';
+import { User } from './models/user.model.js';
+import { Token } from './models/token.model.js';
 import cors from 'cors';
 
 const app = express();
@@ -15,35 +18,25 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc(options)));
 app.use(cors());
 
-app.get('/users', (req, res) => {
-    const result = [
-        {
-            email: 'aaa@gmail.com',
-            name: 'aaa',
-            phone: '010-1234-5678',
-            personal: '220110-2222222',
-            prefer: 'https://naver.com',
-        },
-        {
-            email: 'aaa@gmail.com',
-            name: 'aaa',
-            phone: '010-1234-5678',
-            personal: '220110-2222222',
-            prefer: 'https://naver.com',
-        },
-        {
-            email: 'aaa@gmail.com',
-            name: 'aaa',
-            phone: '010-1234-5678',
-            personal: '220110-2222222',
-            prefer: 'https://naver.com',
-        },
-    ];
+app.get('/users', async (req, res) => {
+    const result = await User.find();
 
     res.send(result);
 });
 
-app.get('/starbucks', (req, res) => {
+app.post('/users', async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        pwd: req.body.pwd,
+        phone: req.body.phone,
+        prefer: req.body.prefer,
+    });
+
+    await user.save();
+});
+
+app.get('/starbucks', async (req, res) => {
     const result = [
         { name: '아메리카노', kcal: 5 },
         { name: '아메리카노', kcal: 5 },
@@ -56,18 +49,31 @@ app.get('/starbucks', (req, res) => {
         { name: '아메리카노', kcal: 5 },
     ];
 
-    res.send(result);
+    const user = await User.find();
+
+    res.send(user);
 });
 
-app.post('/tokens/phone', (req, res) => {
+app.post('/tokens/phone', async (req, res) => {
     const myphone = req.body.qqq;
 
     const isValid = checkPhone(myphone);
     if (isValid === false) return;
 
     const mytoken = getToken();
+    const isToken = await Token.exists({ phone: myphone });
 
-    sendTokenToSMS(myphone, mytoken);
+    if (!isToken) {
+        const token = new Token({
+            token: mytoken,
+            phone: myphone,
+        });
+        await token.save();
+    } else {
+        await Token.updateOne({ phone: myphone }, { token: mytoken });
+    }
+
+    // sendTokenToSMS(myphone, mytoken);
     res.send('Verification completed!!!');
 });
 
@@ -82,6 +88,11 @@ app.post('/email', (req, res) => {
     sendTemplateToEmail(email, myTemplate, name);
     res.send('Registration completed!!!');
 });
+
+mongoose
+    .connect('mongodb://my-database:27017/mydocker')
+    .then(() => console.log('db connect success.'))
+    .catch(() => console.log('db connect fail.'));
 
 app.listen(5000, () => {
     console.log(`http://localhost:5000/`);
