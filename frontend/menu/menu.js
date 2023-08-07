e881e3453994a6e98ed798624345577e3ad4076f
youmin <105027461+futureyp@users.noreply.github.com>
2023-08-07 03:05:33 -0700

    front end

--- frontend/menu/menu.js
+++ frontend/menu/menu.js
@@ -1,33 +1,30 @@
-
-const  getCoffee = () => {
-
-  axios
-  .get('http://localhost:5000/starbucks')
-  .then((res) => {
-    const menu = res.data;
-    menu.map((index)=>{createMenuCard(index)});
-  });
-
-}
+const getCoffee = () => {
+    axios.get('http://localhost:5000/starbucks').then((res) => {
+        const menu = res.data;
+        menu.map((index) => {
+            createMenuCard(index);
+        });
+    });
+};
 
 const createMenuCard = (data) => {
-  const menuCardWrapper = document.createElement('div')
-  menuCardWrapper.className = 'Menu_Card_Wrapper'
+    const menuCardWrapper = document.createElement('div');
+    menuCardWrapper.className = 'Menu_Card_Wrapper';
 
-  const menuCardImgBox = document.createElement('div')
-  menuCardImgBox.className = 'Menu_Card_ImgBox'
+    const menuCardImgBox = document.createElement('div');
+    menuCardImgBox.className = 'Menu_Card_ImgBox';
 
-  const menuCardName = document.createElement('div')
-  menuCardName.className = 'Menu_Card_Name'
-  menuCardName.textContent = data?.name || 'meun name'
+    const menuCardName = document.createElement('div');
+    menuCardName.className = 'Menu_Card_Name';
+    menuCardName.textContent = data?.name || 'meun name';
 
-  const menuCardInfo = document.createElement('div')
-  menuCardInfo.className = 'Menu_Card_Info'
-  menuCardInfo.textContent = data?.kcal || 'kcal'
+    const menuCardInfo = document.createElement('div');
+    menuCardInfo.className = 'Menu_Card_Info';
+    menuCardInfo.textContent = data?.kcal || 'kcal';
 
-  const menuWrapper = document.querySelector('#Menu_Background')
-  menuCardWrapper.appendChild(menuCardImgBox)
-  menuCardWrapper.appendChild(menuCardName)
-  menuCardWrapper.appendChild(menuCardInfo)
-  menuWrapper.appendChild(menuCardWrapper)
-}
+    const menuWrapper = document.querySelector('#Menu_Background');
+    menuCardWrapper.appendChild(menuCardImgBox);
+    menuCardWrapper.appendChild(menuCardName);
+    menuCardWrapper.appendChild(menuCardInfo);
+    menuWrapper.appendChild(menuCardWrapper);
+};
